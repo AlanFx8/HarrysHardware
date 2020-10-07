ebfce3a1a95722325a4355d3b8be29f168b840a3
Alan Freeman <alanfreeman89@hotmail.co.uk>
2020-10-07 16:09:36 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './reset.css';
+import './app.css';
+
+function App() {
+  return (
+    <div id="app">
+      <p>This is a hardware store.</p>
+    </div>
+  );
+}
+
+export default App;
